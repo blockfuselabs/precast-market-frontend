a87a41eb2dae274fbc7b3650f540f0ae3af76b82
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-21 08:54:01 +0100

    chore: treat create-page as dynamic

--- app/create-market/page.tsx
+++ app/create-market/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const dynamic = 'force-dynamic';
+
 import { MarketCreationForm } from "@/components/market/market-creation-form"
 import Header from "@/components/layout/header"
 
